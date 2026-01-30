a319b5d1a922ff9ea63c049d431e40d88e993230
wilkinsst2-pixel <wilkinsst2@vcu.edu>
2026-01-29 23:02:36 -0500

    Change keys

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ function playNote(key) {
     key.classList.remove('active');
   });
 }
+
+const WHITE_KEYS = ['q', 'w', 'e', 'r', 't', 'y', 'u'];
+const BLACK_KEYS = ['1', '2', '3', '4', '5'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+
+  if (e.repeat) {
+    return;
+  }
+
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+
+});
